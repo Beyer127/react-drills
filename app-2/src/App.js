7bf75bcb70a6d8d964d2b92c69845f183a2b201f
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2020-07-22 16:18:55 -0700

    react-drills-app-2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super()
+
+    this.state = {
+      foods: ['bananas', 'bread', 'lettuce', 'milk', 'cerial']
+    }
+  }
+
+  handleChange(value){
+    this.setState({foods: value})
+  }
+
+  render(){
+    let myFood = this.state.foods.map((e, i) => {
+      return <h2 key={i}>{e}</h2>
+    })
+    return(
+      <div className="App">{myFood}</div>
+    )
+  }
 }
 
-export default App;
+export default App
